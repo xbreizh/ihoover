58d0b59633472b77389a769cffc3c293cf34bd1f
xbreizh <xavier.lamourec@gmail.com>
2023-06-16 21:59:34 +0100

    update logs

--- src/main/java/model/Messages.java
+++ src/main/java/model/Messages.java
@@ -39,10 +39,6 @@ public enum Messages {
         this.value = value;
     }
 
-    public String getValue() {
-        return value;
-    }
-
     @Override
     public String toString() {
         return value;
